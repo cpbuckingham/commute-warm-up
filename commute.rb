8f2801f3bc67225f37e640b4ee488af6fe5efba0
Cameron <cameron.p.buckingham@gmail.com>
2014-08-19 13:20:17 -0600

    finished

--- commute.rb
+++ commute.rb
@@ -1,14 +1,63 @@
 require 'csv'
 
+
+table = CSV.foreach('data/gschool_commute_data.csv', headers: true)
+new = table.map do |row|
+   row.to_h
+end
+data =  new.group_by { |row| row["Person"] }
+
+data["Nate"].each do |row|
+  p "Nate's inbound commute on Week 4 Wednesday was #{row["Inbound"].to_i} minutes" if row["Week"] == "4" && row["Day"] == "Wednesday"
+end
+
+total = 0
+data.each do |k,v|
+  v.each do |row|
+    total += (row["Inbound"].to_i + row["Outbound"].to_i)
+  end
+end
+
+p "Average total commute: #{total/(data.count)/(data["Emily"].count)} minutes"
+
+fastest = []
+
+data.each do |k,v|
+  time = 0
+  v.each do |row|
+    time += (row["Inbound"].to_i + row["Outbound"].to_i)
+  end
+  fastest << [k, time/20]
+end
+
+who_won = fastest.sort_by{|times| times[1]}
+
+p "The shortest commute was #{who_won[0][0]}, her average commute was: #{who_won[0][1]} minutes."
+
+#
+# data =  new.group_by { |row| row["Person"] }
+#
+# data.each do |k,v|
+#   v.each do |row|
+#     row.delete("Person")
+#   end
+# end
+# p data
+
+
+
+# customer[:Nate].each do |row|
+#   p "Nate's inbound commute on Week 4 Wednesday was #{row["Inbound"].to_i} minutes" if row["Week"] == "4" && row["Day"] == "Wednesday"
+# end
+
+# CSV.parse(File.read('data/gschool_commute_data.csv'))
+
 # hash= {}
-commuters = CSV.read('data/gschool_commute_data.csv', headers: true)
+# commuters = CSV.read('data/gschool_commute_data.csv', headers: true)
 # # # CSV.foreach('data/gschool_commute_data.csv', { :col_sep => ';' }) { |row| p row }
 # # p commuters
 #
-#
 # p hash
-
-
 
 # commuters.each do |commuter|
 #   hash[commuter[0]] = commuter.to_h
@@ -36,11 +85,4 @@ commuters = CSV.read('data/gschool_commute_data.csv', headers: true)
 #   p commuter_row["Person"]
 #
 # end
-
-CSV.foreach('data/gschool_commute_data.csv') do |row|
-  p row.inspect
-end
-
-# CSV.parse(File.read('data/gschool_commute_data.csv'))
-
 
