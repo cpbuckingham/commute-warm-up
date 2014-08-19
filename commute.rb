24c433002b72e90ac2d4957315b29589bbeaa4ec
Cameron <cameron.p.buckingham@gmail.com>
2014-08-19 10:48:33 -0600

    couldnt hash it out

--- commute.rb
+++ commute.rb
@@ -1,22 +1,46 @@
 require 'csv'
 
+# hash= {}
+commuters = CSV.read('data/gschool_commute_data.csv', headers: true)
+# # # CSV.foreach('data/gschool_commute_data.csv', { :col_sep => ';' }) { |row| p row }
+# # p commuters
+#
+#
+# p hash
 
 
-commuters = CSV.read('data/gschool_commute_data.csv', headers:true)
 
-["Person" ,"Week" ,"Day" ,"Mode" ,"Inbound" ,"Outbound" ,"Distance"]
-
-
-# CSV.foreach('gschool_commute_data.csv') do |row|
-#   puts row.inspect
+# commuters.each do |commuter|
+#   hash[commuter[0]] = commuter.to_h
+# end
+# hash.each do |key, value|
+#   p value
+#   p key
 # end
 
+# CSV.foreach('data/gschool_commute_data.csv') do |row|
+#     p hash[row[0]]
+# end
 
-commuters.each do |commuter_row|
-  p commuter_row
+# people.each do |person|
+#   hash[person[0]] = person
+# end
+#
+# p hash
+# commuters.each do |commuter_row|
+#   p commuter_row
+#
+# end
 
+# commuters.each do |commuter_row|
+#   p commuter_row["Person"]
+#
+# end
+
+CSV.foreach('data/gschool_commute_data.csv') do |row|
+  p row.inspect
 end
 
+# CSV.parse(File.read('data/gschool_commute_data.csv'))
 
 
- 
